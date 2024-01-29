38fe490509faf93c656ccd0200a1a8f22e619a12
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-01-29 14:43:12 +0000

    Creating Navagation Herarchy

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Bruna Mello on 29/01/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
