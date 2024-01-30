b5c576dd32d3d60754b9eac14d35bd071215dcd7
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-01-30 21:40:07 +0000

    Creating Edit View

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Bruna Mello on 30/01/2024.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
